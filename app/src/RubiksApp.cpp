89a2d9221278dc4ed68d61c3264606c7f16bca70
marko <marko03.vucic@gmail.com>
2026-09-20 20:33:15 +0200

    Deferred shading addition

--- app/src/RubiksApp.cpp
+++ app/src/RubiksApp.cpp
@@ -1,13 +1,16 @@
 #include <app/MainController.hpp>
 #include <app/RubiksApp.hpp>
+#include <engine/graphics/GBuffer.hpp>
 #include <engine/graphics/PostProcessController.hpp>
 
 namespace app {
 void RubiksApp::app_setup() {
     auto main_controller = register_controller<MainController>();
     auto post_process = register_controller<engine::graphics::PostProcessController>();
+    auto gbuffer = register_controller<engine::graphics::GBuffer>();
     main_controller->after(engine::core::Controller::get<engine::core::EngineControllersEnd>());
     post_process->after(engine::core::Controller::get<engine::core::EngineControllersEnd>());
+    gbuffer->after(engine::core::Controller::get<engine::core::EngineControllersEnd>());
 }
 }// namespace app
 
